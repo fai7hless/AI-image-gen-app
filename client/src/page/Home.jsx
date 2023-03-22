987ea8abac83279d4b2aa570912250e1b4b7fdd3
fai7hless <vasilpopovxd@gmail.com>
2023-03-22 18:00:26 +0200

    new urls

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dall-e-image-gen-site.onrender.com/api/v1/post', {
+      const response = await fetch('https://xdddddd-28js.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
